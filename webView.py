0682bea136c0b76ea2ba61b3fc48e22a35415469
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2023-02-14 23:08:23 -0500

    bad avance

--- webView.py
+++ webView.py
@@ -10,26 +10,32 @@ app = Flask(__name__)
 def get():
     return render_template('index.html',data=vert)
 
-@app.route('/table')
-def table():
-    if 'table_data' not in session:  
-        ls = readColumn('data.csv','Elo')
+@app.route('/table/<int:ad>')
+def table(ad): 
+    ls = readColumn('data.csv','Elo')
 
-        root = BSTNode(ls[0])
+    root = BSTNode(ls[0])
+    ls.append(ad)
+    insertMany(root, ls)
 
-        insertMany(root, ls)
+    resPre, resIn, resPs = [], [], []
+    preOr, preIn, prePs = orderAll(root, resPre, 'PRE'), orderAll(root, resIn, 'IN'), orderAll(root, resPs, 'POST')
 
-        resPre, resIn, resPs = [], [], []
-        preOr, preIn, prePs = orderAll(root, resPre, 'PRE'), orderAll(root, resIn, 'IN'), orderAll(root, resPs, 'POST')
+    vert = {}
+    map(resPre)
+    indexMap(resIn, resPre,vert)
+    
+    return render_template('table.html',data=vert)
 
-        vert = {}
-        map(resPre)
-        indexMap(resIn, resPre,vert)
-        
-        session['table_data'] = cook
-  
-    return render_template('table.html',data=vert, cook=session['table_data'])
+@app.route("/test")
+def test():
+    return render_template('ajax.html')
+
+@app.route("/get_data")
+def get_data():
+    text = request.args.get('text')
+    return text
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True, threaded=True)
+    app.run(port=8080, debug=True)
 
